12cd1eff0c5c7943f11d8c4d1e8cf982430f2983
00752-tech <passiveincomestreaming@gmail.com>
2025-07-02 17:27:03 -0700

    Update workout-plan-generator.tsx

--- components/workout-plan-generator.tsx
+++ components/workout-plan-generator.tsx
@@ -2,6 +2,7 @@
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Sparkles, ArrowRight } from 'lucide-react'
+import React, { useState } from 'react'
 
 const OldSchoolNewBodyAd = () => {
   const handleCTAClick = () => {
