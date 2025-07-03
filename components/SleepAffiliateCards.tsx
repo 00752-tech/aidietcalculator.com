fa4850ef6ceb2986a312859388bf5b5111108305
00752-tech <passiveincomestreaming@gmail.com>
2025-07-03 11:15:12 -0700

    Update SleepAffiliateCards.tsx

--- components/SleepAffiliateCards.tsx
+++ components/SleepAffiliateCards.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-import { Card, CardHeader, CardContent, CardTitle } from '@/components/ui/card'
+import React from 'react'
 import { Sparkles, Zap, ArrowRight, ShieldCheck } from 'lucide-react'
+import { Card, CardHeader, CardContent, CardTitle } from '@/components/ui/card'
+
 
 export default function SleepAffiliateCards() {
   return (
